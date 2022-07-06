fa8f564beba46c4f7d6e52d92a41c6746a6b83fc
Sarah Lopez <slopez323@gmail.com>
2022-07-06 15:05:30 -0400

    connected to db

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 var blogsRouter = require("./routes/blogs");
-app.use("/blogs", blogsRouter);
+var { mongoConnect } = require("./mongo.js");
+mongoConnect();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,8 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
+app.options("*", cors());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/blogs", blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
+const { blogsDB } = require("../mongo");
+
 router.get("/hello-blogs", function (req, res, next) {
   res.json({ message: "hello from express" });
+});
+
+router.get("/all-blogs", async function (req, res, next) {
+  try {
+    const collection = await blogsDB().collection("posts");
+    const posts = await collection.find({}).toArray();
+    res.json(posts);
+  } catch (e) {
+    res.status(500).send("Error fetching posts.");
+  }
 });
 
 module.exports = router;
